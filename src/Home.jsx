b197d7375cfc0014259f552fa865e3d53721a837
faustoche <faustine.crocq@gmail.com>
2026-06-30 09:10:17 +0200

    add: new home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Globe, FolderOpen, MoveRight } from 'lucide-react'
+import './style.css';
 
 export default function Home() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', backgroundColor: '#1a1a1a', color: 'white' }}>
-      <h1>Faustoche</h1>
-      <div style={{ display: 'flex', gap: '20px', marginTop: '20px' }}>
-        <Link to="/map" style={{ padding: '15px 30px', background: '#3b82f6', color: 'white', textDecoration: 'none', borderRadius: '5px' }}>
-          WIP WORLD
+    <div className="home">
+
+      <header className="home-header">
+        <div className="home-header_name">
+          Faustine Crocq
+        </div>
+
+        <nav className="home-header_links">
+          <Link to="http://linkedin.com/in/faustine-crocq-7a1994150">Linkedin</Link>
+          <Link to="https://github.com/faustoche">Github</Link>
+        </nav>
+      </header>
+
+
+
+      <section className="home-hero">
+        <h1 className="home-hero_title">Faustoche</h1>
+      </section>
+
+      {/*  BOUTONS  */}
+      <section className="home-buttons">
+        <Link to="/map" className="home-button home-button--primary">
+          <Globe size={23}/>
+          <span>Dive into my 3D world</span>
+          <MoveRight size={18}/>
         </Link>
-        <Link to="/projects" style={{ padding: '15px 30px', background: '#10b981', color: 'white', textDecoration: 'none', borderRadius: '5px' }}>
-          PROJECT
+        <Link to="/projects" className="home-button home-button--secondary">
+        <FolderOpen size={23}/>
+        <span>See previous projects</span>
+        <MoveRight size={18}/>
         </Link>
-      </div>
+      </section>
+
+      {/*  FOOTER  */}
+      <footer className="home-footer">
+        <p>© {new Date().getFullYear()} Faustine</p>
+      </footer>
+
     </div>
   );
 }
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default function Projects() {
   return (
-    <div style={{ padding: '40px', backgroundColor: '#1a1a1a', color: 'white', height: '100vh' }}>
-      <h1>Mes Projets</h1>
-      <p>Ici, vous mettrez vos projets classiques.</p>
+    <div style={{ padding: '40px', backgroundColor: '#ffffff', color: 'white', height: '100vh' }}>
+      <h1>PREVIOUS PROJECTS</h1>
+      <p>ici cadre + projets</p>
       <Link to="/" style={{ color: '#3b82f6' }}>Retour à l'accueil</Link>
     </div>
   );
